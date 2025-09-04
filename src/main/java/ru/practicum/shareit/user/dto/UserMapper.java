bfe5f30e9c9e086936a5ea66035073165f898570
Sergey Shihovcev <Shihovcev.sv@yandex.ru>
2025-09-04 15:20:08 +0300

    Подготовленны основные классы

---
+++ src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
@@ -0,0 +1,15 @@
+package ru.practicum.shareit.user.dto;
+
+import ru.practicum.shareit.user.User;
+
+public class UserMapper {
+    public static UserDto toUserDto(User user) {
+        if (user == null) return null;
+        UserDto dto = new UserDto();
+        dto.setId(user.getId());
+        dto.setName(user.getName());
+        dto.setEmail(user.getEmail());
+        return dto;
+    }
+}
+
