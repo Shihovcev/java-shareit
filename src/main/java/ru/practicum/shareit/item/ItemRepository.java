f0950021b1079073a2a0807a3f2468e144e01a0b
Sergey Shihovcev <Shihovcev.sv@yandex.ru>
2025-09-05 13:56:42 +0300

    Исправление чекстайла

--- src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -5,10 +5,16 @@ import java.util.List;
 
 public interface ItemRepository {
     Item save(Item item);
+
     Item update(Long id, Item item);
+
     Item findById(Long id);
+
     List<Item> findAllByOwnerId(Long ownerId);
+
     List<Item> search(String text);
+
     List<Item> findAll();
+
     void delete(Long id);
 }
--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -5,9 +5,13 @@ import java.util.List;
 
 public interface ItemService {
     ItemDto create(ItemDto itemDto, Long ownerId);
+
     ItemDto update(Long itemId, ItemDto itemDto, Long ownerId);
+
     ItemDto getById(Long itemId);
+
     List<ItemDto> getAllByOwner(Long ownerId);
+
     List<ItemDto> search(String text);
 }
 
