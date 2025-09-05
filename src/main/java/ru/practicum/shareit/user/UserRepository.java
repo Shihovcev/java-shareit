8134205d0c474c8b3f30d48b90471f26094af74e
Sergey Shihovcev <Shihovcev.sv@yandex.ru>
2025-09-05 13:52:08 +0300

    Первое ревью 14 спринта

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 public interface UserRepository {
     User save(User user);
+
     User update(Long id, User user);
+
     User findById(Long id);
+
     List<User> findAll();
+
     void delete(Long id);
+
     boolean existsByEmail(String email);
 }
 
